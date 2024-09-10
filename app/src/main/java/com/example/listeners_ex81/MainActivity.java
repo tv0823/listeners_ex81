eaff03bfcedcbb1c62305c345a6615286f8a4067
tv0823 <tv0823@bs.amalnet.k12.il>
2024-09-10 15:43:25 +0300

    first page done -> blue and red button

--- app/src/main/java/com/example/listeners_ex81/MainActivity.java
+++ app/src/main/java/com/example/listeners_ex81/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.listeners_ex81;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +12,33 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button blueBtn, redBtn;
+    int blueCounter, redCounter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        blueBtn = findViewById(R.id.blueBtn);
+        redBtn = findViewById(R.id.redBtn);
+        blueCounter = 0;
+        redCounter = 0;
+
+        blueBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                blueCounter ++;
+            }
+        });
+
+        redBtn.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                redCounter ++;
+                return true;
+            }
+        });
+
     }
 }
